aa3a6e81e5826542f1576f89ea344c44e460e9da
Rubén Bonilla <65454960+DrLayton22@users.noreply.github.com>
2025-02-06 18:59:27 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,19 +1,21 @@
 def buildJar() {
     echo "building the application..."
-    sh 'mvn package'
+    sh 'mvn package'  // Usamos Maven para empaquetar el proyecto
 } 
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'drlay/docker-grovvy', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh 'docker push drlay/docker-grovvy:grovvy-1.1.1'
+    withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'  // Construimos la imagen Docker
+        sh "echo $PASS | docker login -u $USER --password-stdin"  // Login en Docker Hub
+        sh 'docker push nanajanashia/demo-app:jma-2.0'  // Subimos la imagen a Docker Hub
     }
 } 
 
 def deployApp() {
     echo 'deploying the application...'
+    // Aquí puedes agregar el código necesario para desplegar la aplicación
+    // Por ejemplo, invocar un Kubernetes o un comando de Docker para ejecutar el contenedor.
 } 
 
 return this
